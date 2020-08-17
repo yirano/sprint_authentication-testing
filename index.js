4050ed508d96213c42d0df4eea29c52aa8133407
yirano <yirano@protonmail.ch>
2020-08-16 21:11:10 -0400

    Reorganize folder structure -- get rid of the annoying/confusing server.js sitting in the middle

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const server = require('./api/server.js');
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
-const PORT = process.env.PORT || 3300;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
+const authenticate = require('./auth/authenticate-middleware.js')
+const authRouter = require('./auth/auth-router.js')
+const jokesRouter = require('./jokes/jokes-router.js')
+
+const server = express()
+const PORT = process.env.PORT || 3300
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+server.use('/api/auth', authRouter)
+// server.use('/api/jokes', authenticate, jokesRouter)
+server.use('/api/jokes', jokesRouter)
+
+if (!module.parent) {
+	server.listen(PORT, () => {
+		console.log(`\n=== Server listening on port ${PORT} ===\n`)
+	})
+}
+
+module.exports = server
